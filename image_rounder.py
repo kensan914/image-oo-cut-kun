5ecf46b742ccb124f21b05848f1a33bf3e224bf5
toriumi <ken.tennis914@gmail.com>
2021-08-26 19:18:34 +0900

    complete 2.x

--- image_rounder.py
+++ image_rounder.py
@@ -1,9 +1,9 @@
 from ImageEditor import ImageEditor
 
 
-def round_image(input_img_path, output_folder_path = None):
+def round_image(input_img_path, output_folder_path = None, resize_px=300):
     image_editor = ImageEditor(input_img_path)
-    image_editor.compress(resize_px=300)
+    image_editor.compress(resize_px=resize_px)
     image_editor.trim_square()
     image_editor.mask_circle_transparent()
     image_editor.save(output_folder_path)
